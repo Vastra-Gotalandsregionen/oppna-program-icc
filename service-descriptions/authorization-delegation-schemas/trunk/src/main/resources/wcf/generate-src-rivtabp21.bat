fb8205e08254c12ad563ab45f02547756a2cdbd0
magnus.larsson.ml@gmail.com <magnus.larsson.ml@gmail.com@0c0c8749-ea18-a50c-78a3-2a99b7006bef>
2012-04-24 15:18:37 +0000

    Added license header

--- service-descriptions/authorization-delegation-schemas/trunk/src/main/resources/wcf/generate-src-rivtabp21.bat
+++ service-descriptions/authorization-delegation-schemas/trunk/src/main/resources/wcf/generate-src-rivtabp21.bat
@@ -3,21 +3,13 @@
 	@REM Generates c# WCF service contracts (interface), client proxies and wcf config
 	@REM file for the WSDLs + XML Schemas of the service domain, using .Net WCF tool svcuti.exe
 	@REM ---------------------------------------------------------------------------------
-	@REM Licensed to the Apache Software Foundation (ASF) under one
-	@REM or more contributor license agreements. See the NOTICE file
-	@REM distributed with this work for additional information
-	@REM regarding copyright ownership. Inera AB licenses this file
-	@REM to you under the Apache License, Version 2.0 (the
-	@REM "License"); you may not use this file except in compliance
-	@REM with the License. You may obtain a copy of the License at
-	@REM
-	@REM http://www.apache.org/licenses/LICENSE-2.0
-	@REM Unless required by applicable law or agreed to in writing,
-	@REM software distributed under the License is distributed on an
-	@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-	@REM KIND, either express or implied. See the License for the
-	@REM specific language governing permissions and limitations
-	@REM under the License.
+    @REM Copyright 2010 Västra Götalandsregionen This library is free software; you can redistribute it and/or modify it
+    @REM under the terms of version 2.1 of the GNU Lesser General Public License as published by the Free Software
+    @REM Foundation. This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+    @REM even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+    @REM Public License for more details. You should have received a copy of the GNU Lesser General Public License along
+    @REM with this library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+    @REM 02111-1307 USA
 	@REM ---------------------------------------------------------------------------------
 	CD ..
 	
